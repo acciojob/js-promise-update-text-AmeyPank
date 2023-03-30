94409596564fbd92bf8f6b6296b5c2854a504dd1
Amey  Pankanti <86099941+AmeyPank@users.noreply.github.com>
2023-03-30 19:06:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateOutput() {
+            const outputElement = document.getElementById("output");
+
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve("Hello, world!");
+                }, 1000);
+            }).then((result) => {
+                outputElement.innerText = result;
+            });
+        }
+
+        updateOutput();
